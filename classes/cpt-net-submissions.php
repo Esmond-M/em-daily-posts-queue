2646e66678c05fb10935086810b47e5483490342
Esmond <esmondmccain@gmail.com>
2025-06-08 15:00:44 -0500

    added custom role and cap

--- classes/cpt-net-submissions.php
+++ classes/cpt-net-submissions.php
@@ -13,13 +13,17 @@ if (!class_exists('cpt_net_submission')) {
         {
 
 			add_action( 'init', [$this, 'em_daily_posts_register_cpts' ]  );
+			add_action( 'init', [$this, 'net_submission_role' ]  );
+			add_action( 'init', [$this, 'net_submission_cap' ]  );
+
+	
 
         }
 
 		public static function em_daily_posts_register_cpts() {
 
 			/**
-			 * Post Type: portfolio.
+			 * Post Type: Net submissions.
 			 */
 		
 			$labels = array(
@@ -43,7 +47,24 @@ if (!class_exists('cpt_net_submission')) {
 				'show_in_nav_menus'     => true,
 				'delete_with_user'      => false,
 				'exclude_from_search'   => true,
-				'capability_type'       => 'post',
+				'capability_type'       => 'net_submission',
+				'capabilities' => array(
+					'edit_post' => 'edit_net_submission',
+					'read_post' => 'read_net_submission',
+					'delete_post' => 'delete_net_submission',
+					'edit_posts' => 'edit_net_submissions',
+					'edit_others_posts' => 'edit_others_net_submissions',
+					'delete_posts' => 'delete_net_submissions',
+					'publish_posts' => 'publish_net_submissions',
+					'read_private_posts' => 'read_private_net_submissions',
+					'read' => 'read',
+					'delete_private_posts' => 'delete_private_net_submissions',
+					'delete_published_posts' => 'delete_published_net_submissions',
+					'delete_others_posts' => 'delete_others_net_submissions',
+					'edit_private_posts' => 'edit_private_net_submissions',
+					'edit_published_posts' => 'edit_published_net_submissions',
+					'create_posts' => 'edit_net_submissions'
+				),
 				'map_meta_cap'          => true,
 				'hierarchical'          => false,
 				'can_export'            => true,
@@ -59,7 +80,61 @@ if (!class_exists('cpt_net_submission')) {
 		
 			register_post_type( 'net_submission', $args );
 	
-	} 
+	}
+	
+    public function net_submission_role() {
+
+        add_role('net_submission_role', 'Net Submitter', array(
+            'read' => true,
+            'create_posts' => false,
+            'edit_posts' => false,
+		    'edit_posts' => false,
+            'edit_others_posts' => false,
+            'publish_posts' => false,
+            'manage_categories' => false,
+
+        ));
+		
+    }
+	
+	public function net_submission_cap() {
+
+		$net_submission_role = get_role( 'net_submission_role' );
+        $admins = get_role( 'administrator' );
+
+		$admins->add_cap( 'edit_net_submission' ); 
+		$admins->add_cap( 'edit_net_submission' ); 
+		$admins->add_cap( 'read_net_submission' ); 
+		$admins->add_cap( 'delete_net_submission' ); 
+		$admins->add_cap( 'edit_net_submissions' ); 
+		$admins->add_cap( 'edit_others_net_submissions' ); 
+		$admins->add_cap( 'delete_net_submissions' ); 
+		$admins->add_cap( 'publish_net_submissions' ); 
+		$admins->add_cap( 'read_private_net_submissions' ); 
+		$admins->add_cap( 'delete_private_net_submissions' ); 
+		$admins->add_cap( 'read_private_net_submissions' ); 
+		$admins->add_cap( 'delete_published_net_submissions' ); 
+		$admins->add_cap( 'delete_others_net_submissions' ); 
+		$admins->add_cap( 'edit_private_net_submissions' ); 
+		$admins->add_cap( 'edit_published_net_submissions' ); 
+
+		$net_submission_role->add_cap( 'edit_net_submission' ); 
+		$net_submission_role->add_cap( 'edit_net_submission' ); 
+		$net_submission_role->add_cap( 'read_net_submission' ); 
+		$net_submission_role->add_cap( 'delete_net_submission' ); 
+		$net_submission_role->add_cap( 'edit_net_submissions' ); 
+		$net_submission_role->add_cap( 'edit_others_net_submissions' ); 
+		$net_submission_role->add_cap( 'delete_net_submissions' ); 
+		$net_submission_role->add_cap( 'publish_net_submissions' ); 
+		$net_submission_role->add_cap( 'read_private_net_submissions' ); 
+		$net_submission_role->add_cap( 'delete_private_net_submissions' ); 
+		$net_submission_role->add_cap( 'read_private_net_submissions' ); 
+		$net_submission_role->add_cap( 'delete_published_net_submissions' ); 
+		$net_submission_role->add_cap( 'delete_others_net_submissions' ); 
+		$net_submission_role->add_cap( 'edit_private_net_submissions' ); 
+		$net_submission_role->add_cap( 'edit_published_net_submissions' ); 
+
+    }	
 
 } // Closing bracket for classes
 
