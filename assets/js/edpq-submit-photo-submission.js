9efbe19366248f6edd72f361b1b8d8f6fc4d06e4
Esmond <esmondmccain@gmail.com>
2025-08-31 15:43:44 -0500

    refactored js code
    Button Enable/Disable Logic:
    I only need to target button and input[type='submit'] directly inside the form.
    
    Error Message Handling:
    If the form is submitted multiple times and fails, multiple error divs could be appended. Remove any existing error message before appending a new one.
    
    Loader Accessibility:
    Add aria-live="polite" to the loader div for better accessibility.
    
    General Cleanup:
    Use chaining and cache jQuery selectors for performance.

--- assets/js/edpq-submit-photo-submission.js
+++ assets/js/edpq-submit-photo-submission.js
@@ -1,43 +1,41 @@
 jQuery(document).ready(function ($) {
-    jQuery( "#new_post" ).submit(function( event ) {
-        event.preventDefault();
-        jQuery("#new_post button").prop( "disabled", true ); // disable all form buttons
-        jQuery("#new_post button input[type='submit']").prop( "disabled", true ); // disable all form buttons
-        jQuery('.edpq-ajax-loader').css('display', 'block'); // show existing loader
+    var $form = $("#new_post");
+    var $loader = $(".edpq-ajax-loader");
 
-    // Serialize the data in the form
-    var formData = new FormData(jQuery("#new_post")[0]); 
-    //console.log(serializedData);
-        jQuery.ajax({
+    $form.submit(function (event) {
+        event.preventDefault();
+        $form.find("button, input[type='submit']").prop("disabled", true);
+        $loader.css("display", "block");
+
+        // Remove any previous error message
+        $("#edpq-connect-error").remove();
+
+        var formData = new FormData($form[0]);
+        $.ajax({
             type: "POST",
             url: ajax_form_post_new_net_photo_submission.ajaxurl_form_post_new_net_photo_submission,
             dataType: "html",
-			processData: false,
-			contentType: false,
+            processData: false,
+            contentType: false,
             data: formData,
-            success: function(responseText){ 
-                setTimeout(
-                    function() {
-                        jQuery('.edpq-ajax-loader').css('display', 'none'); // hide loader
-                        jQuery('#new_post').after(responseText);
-                        jQuery('#new_post').remove();
-                        var $modal = jQuery('.newpost-success');
-                        jQuery([window.top.document.documentElement, window.top.document.body]).animate({scrollTop: $modal.offset().top - ($modal.width() / 2)}, 500);
-                    },
-                    0
-                );
-
-          },
-            error: function(jqXHR, textStatus, errorThrown) {
-                jQuery('.edpq-ajax-loader').css('display', 'none'); // hide loader
-				jQuery("#new_post button").prop( "disabled", false ); // enable all form buttons
-				jQuery("#new_post input[type='submit']").prop( "disabled", false ); // enable all form buttons
-                jQuery("#new_post").append('<div id="edpq-connect-error">Connection Error</div>');
-
+            success: function (responseText) {
+                setTimeout(function () {
+                    $loader.css("display", "none");
+                    $form.after(responseText);
+                    $form.remove();
+                    var $modal = $(".newpost-success");
+                    if ($modal.length > 0) {
+                        $([window.top.document.documentElement, window.top.document.body])
+                            .animate({ scrollTop: $modal.offset().top - ($modal.width() / 2) }, 500);
+                    }
+                }, 0);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $loader.css("display", "none");
+                $form.find("button, input[type='submit']").prop("disabled", false);
+                $form.append('<div id="edpq-connect-error">Connection Error</div>');
                 console.log(JSON.stringify(jqXHR) + " :: " + textStatus + " :: " + errorThrown);
             }
-
-        });// ajax end
-});
-
+        });
+    });
 });
