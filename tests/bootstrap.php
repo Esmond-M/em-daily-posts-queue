7a124cb4bbf7a40cb6dcbbfd30eb84a62b514367
Esmond <esmondmccain@gmail.com>
2025-09-02 19:48:53 -0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,15 +3,22 @@
  * PHPUnit bootstrap file
  */
 
-// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
-require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+// Composer autoloader
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-// Give access to tests_add_filter() function.
-require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+// WP test functions
+require_once getenv('WP_PHPUNIT__DIR') . '/includes/functions.php';
 
-tests_add_filter( 'muplugins_loaded', function() {
+tests_add_filter('muplugins_loaded', function() {
     // test set up, plugin activation, etc.
-} );
+});
 
-// Start up the WP testing environment.
-require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
+// WP testing environment
+require getenv('WP_PHPUNIT__DIR') . '/includes/bootstrap.php';
+
+// Manually require plugin class files for tests
+require_once dirname(__DIR__) . '/classes/class-cron-event-timer.php';
+require_once dirname(__DIR__) . '/classes/class-photo-submission-queue-manager.php';
+require_once dirname(__DIR__) . '/classes/class-photo-submission-ajax.php';
+require_once dirname(__DIR__) . '/classes/class-photo-submission-utils.php';
+// Add other class files as needed
